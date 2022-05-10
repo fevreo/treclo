b95b29e7dc61ab5249857f13f113f523bb7d58e8
yuy-dev <ykoba.web@gmail.com>
2022-05-10 18:58:02 +0900

    Add Laravel Passport

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', [UserController::class, 'login']);
+Route::post('register', [UserController::class, 'register']);
+
+Route::group(['middleware' => 'auth:api'], function () {
+	Route::resource('/category', CategoryController::class);
+	Route::resource('/task', TaskController::class);
+	Route::get('/category/{category}/tasks', [CategoryController::class, 'tasks']);
 });
