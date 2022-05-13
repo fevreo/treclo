29f879ec8368e26858286e7172d4b76e72ddc3cb
yuy-dev <ykoba.web@gmail.com>
2022-05-13 20:12:05 +0900

    Add Vue.js

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SinglePageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/{any}', [SinglePageController::class, 'index'])->where('any', '.*');
 
 Route::get('/', function () {
     return view('welcome');
